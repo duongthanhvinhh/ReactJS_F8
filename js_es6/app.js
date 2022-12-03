307b20f4719b50f1415ba09004c4238fe8876fed
unknown <duongvinh1706@gmail.com>
2022-12-03 15:02:58 -0800

    module

--- js_es6/app.js
+++ js_es6/app.js
@@ -7,9 +7,15 @@ import logger, {
     TYPE_TRACE,
 } from './logger.js';
 
+//Can use below way to import all constant:
+import * as constants from './constants.js'
+console.log(constants);
+
 // console.log(logger);
 // console.log(typeof logger);
+logger("Debug: Replacing default configurations inside config.properties",TYPE_DEBUG);
 logger("An error occurred",'error');
 logger("Warning: Modify this module can potentially lead to system down",TYPE_WARN);
 logger("Info: Console is running... ",TYPE_INFO)
 logger("Debug: Replacing default configurations inside config.properties",TYPE_DEBUG);
+logger("An error occurred",TYPE_ERROR);
--- js_es6/constants.js
+++ js_es6/constants.js
@@ -1,15 +1,6 @@
-//Export without default
 export const TYPE_LOG = 'log';
 export const TYPE_WARN = 'warn';
 export const TYPE_ERROR = 'error';
 export const TYPE_DEBUG = 'debug';
 export const TYPE_INFO = 'info';
 export const TYPE_TRACE = 'trace';
-
-
-
-function logger(log,type='log'){
-    console[type](log);
-}
-//Export with default
-export default logger;
